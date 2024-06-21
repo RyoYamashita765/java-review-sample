9de8e9a1c3b60d07c7f29857ec7598cf2b23aceb
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-21 15:58:30 +0900

    Remvoe the "for" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,9 +3,5 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
-		for (int i = 1; i <= 10; i++) {
-			System.out.println("i^2 = " + i * i);
-		}
 	}
 }
