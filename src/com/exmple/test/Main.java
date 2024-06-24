a71e511b7cf13b6963f1523741e1de821f2311c6
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 15:14:56 +0900

    Add "while" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,6 +2,10 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
-
+		int answer = 0;
+		while (answer != 42) {
+			System.out.println("answer: " + answer);
+			answer++;
+		}
 	}
 }
