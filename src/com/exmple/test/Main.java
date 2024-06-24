9d33c7040638e73600967f82572426ce3de899f2
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 14:30:04 +0900

    Delete multiple lines in "for" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,8 +3,6 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		for (int i = 0; i < 20; i++) {
-			System.out.println("Hello World! [" + i + "]");
-			System.out.println("i * 2= " + i);
 			System.out.println("i * i = " + i * i);
 		}
 	}
