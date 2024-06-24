6ece8cb04eb2cc77586a45ebf523da3d87d1389d
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 13:56:21 +0900

    Update "switch" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,8 +3,8 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
-		switch (answer) {
-			case 42:
+		switch (answer - 1) {
+			case 41:
 				System.out.println("The answer is 42.");
 				break;
 			default:
