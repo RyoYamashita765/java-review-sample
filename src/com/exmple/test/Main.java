9a7a9e4d5a952d7d3108c0709c3b0e9a985e30a6
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 15:16:22 +0900

    Update multiple lines in "while" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -4,8 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		int answer = 0;
 		while (answer < 42) {
-			System.out.println("The answer is " + answer);
-			answer++;
+			System.out.println("The answer: " + answer);
+			answer += 2;
 			if (answer == 42) {
 				System.out.println("The answer is 42!");
 			}
