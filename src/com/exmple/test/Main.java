80e2fede68a3cba57a4cdeddb8798039a33feaa8
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 12:30:15 +0900

    Add "for" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,5 +3,8 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello, World! " + i);
+		}
 	}
 }
