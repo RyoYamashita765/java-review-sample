f77f1f8758ef4fb9cf3749d6391e305dc56f46db
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 14:18:18 +0900

    Update multiple lines

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,8 +2,8 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.print("Lorem ipsum dolor sit amet, ");
-		System.out.println("consectetur adipiscing elit.");
+		System.out.println("Quo usque tandem abutere, Catilina, patientia nostra?");
+		System.out.println("Quam diu etiam furor iste tuus nos eludet?");
+		System.out.println("Quem ad finem sese effrenata iactabit audacia?");
 	}
 }
