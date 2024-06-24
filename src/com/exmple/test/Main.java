cd9c802f8f9853698d0a6368b1f2dcf49b3ebf7a
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 14:17:39 +0900

    Add multiple lines

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,5 +3,7 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.print("Lorem ipsum dolor sit amet, ");
+		System.out.println("consectetur adipiscing elit.");
 	}
 }
