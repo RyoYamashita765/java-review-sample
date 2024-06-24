4672934a411b9e373858ca818289bcba25cebf2b
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 12:24:14 +0900

    Update "if" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,7 +3,7 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
-		if (answer == 42) {
+		if (answer >= 42) {
 			System.out.println("The answer is " + answer);
 		}
 	}
