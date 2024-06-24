3238b21e36a00987f89b35784032837885eb3005
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 14:28:35 +0900

    Update "for" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,8 +2,8 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			System.out.println("Hello World! " + i);
+		for (int i = 0; i < 20; i++) {
+			System.out.println("Hello World! [" + i + "]");
 		}
 	}
 }
