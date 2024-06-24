49d8ff049e99dc3cea61caafc35c86dbdf3724f6
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 13:45:07 +0900

    Delete "for" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,8 +3,5 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
-		for (int i = 0; i < 20; i++) {
-			System.out.println("i = " + i);
-		}
 	}
 }
