90c80c0b1d3de3ca13b60c55be4ed9e1e478a668
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 14:02:18 +0900

    Update one line in "while" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
 		while (answer >= 0) {
-			System.out.println("Hello, World!");
+			System.out.println("Hello, Java World!");
 			System.out.println("The answer is " + answer);
 			answer--;
 		}
