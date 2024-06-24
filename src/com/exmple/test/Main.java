db6ab7fc6e7612bd7fb09e412541964cd502b3de
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 14:18:34 +0900

    Delete multiple lines

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,8 +2,6 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Quo usque tandem abutere, Catilina, patientia nostra?");
-		System.out.println("Quam diu etiam furor iste tuus nos eludet?");
-		System.out.println("Quem ad finem sese effrenata iactabit audacia?");
+
 	}
 }
