92aa12c072757e0e971e7acf75546d5e75746166
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 13:54:36 +0900

    Add "switch" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,5 +3,13 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
+		switch (answer) {
+			case 42:
+				System.out.println("The answer is 42.");
+				break;
+			default:
+				System.out.println("The answer is not 42.");
+				break;
+		}
 	}
 }
