73ec8f10995c3c60ca9fbea3cb41fded320a6549
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 14:58:02 +0900

    Delete "switch" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,13 +2,6 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
-		switch (args.length + 1) {
-		case 1:
-			System.out.println("No arguments");
-			break;
-		default:
-			System.out.println("Arguments:");
-			break;
-		}
+
 	}
 }
