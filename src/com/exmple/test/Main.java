0f1b83795bc8f95f122e0740d19bcdda0638c491
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 13:59:22 +0900

    Delete "switch" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,13 +3,5 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
-		switch (answer - 1) {
-			case 41:
-				System.out.println("The answer is 42.");
-				answer++;
-				break;
-			default:
-				return;
-		}
 	}
 }
