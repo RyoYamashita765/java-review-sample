96a9d08be01d42744a54a519ad02cf067af84a5b
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 15:16:55 +0900

    Delete "while" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,10 +2,6 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
-		int answer = 0;
-		while (answer < 42) {
-			System.out.println("The answer: " + answer);
-			answer += 2;
-		}
+
 	}
 }
