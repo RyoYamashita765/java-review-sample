aa05c39cbeb2b61bca727b9056f7e1321d60e647
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 16:10:25 +0900

    Delete lines from some points

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,14 +2,6 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
-
-
-
 		System.out.println("Hello World!");
-
-
-
-		System.out.println("Duis aute irure dolor in reprehenderit in voluptate ...");
 	}
 }
