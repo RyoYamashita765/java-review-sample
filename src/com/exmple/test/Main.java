edf423d8ec2abf0e3bde60f05dbd8e571c1ef445
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 12:26:31 +0900

    Delete "if" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,8 +3,5 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
-		if (answer >= 42) {
-			System.out.println("42 is less than or equal to the answer");
-		}
 	}
 }
