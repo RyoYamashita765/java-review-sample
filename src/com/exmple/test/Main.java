18bd673af45436362dadd2ab10580d84b5528736
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 14:29:53 +0900

    Update multiple lines in "for" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -4,8 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		for (int i = 0; i < 20; i++) {
 			System.out.println("Hello World! [" + i + "]");
-			System.out.println("i = " + i);
-			System.out.println("i * 2 = " + i * 2);
+			System.out.println("i * 2= " + i);
+			System.out.println("i * i = " + i * i);
 		}
 	}
 }
