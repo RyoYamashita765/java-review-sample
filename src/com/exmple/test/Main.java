2362a92849f7c8bf603baf8ba5b73109c73ee1a1
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 17:42:56 +0900

    Delete "if" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,12 +3,5 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
-		if (args.length >= 1) {
-			System.out.println("========= Arguments =========");
-			for (String arg : args) {
-				System.out.println("  [" + arg + "]");
-			}
-		}
 	}
 }
