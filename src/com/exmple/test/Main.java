6b1d60f42eb3b53fcc270d622e9f40583699c406
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-24 12:26:14 +0900

    Delete one line in "if" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -4,7 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		int answer = 42;
 		if (answer >= 42) {
-			System.out.println("The answer is " + answer);
 			System.out.println("42 is less than or equal to the answer");
 		}
 	}
