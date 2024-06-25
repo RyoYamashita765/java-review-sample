9dae324848b46f24b6b76ff72b50cf0eb4567417
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 15:27:05 +0900

    Remove codes

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,18 +2,6 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
-		if (args.length > 0) {
-			if (args.length > 1) {
-				if (args.length > 2) {
-					System.out.println("Too many arguments");
-				} else {
-					System.out.println("Two arguments");
-				}
-			} else {
-				System.out.println("One argument");
-			}
-		} else {
-			System.out.println("No arguments");
-		}
+
 	}
 }
