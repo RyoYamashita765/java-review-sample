889675d3454d557e874e96529bf0cea7056be5db
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 10:50:52 +0900

    Add file

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,12 +2,6 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
-		Printer printer = new Printer();
-	}
-}
-
-class Printer {
-	public void print() {
-		System.out.println("Hello, Java World!");
+		System.out.println("Hello, Main World!");
 	}
 }
---
+++ src/com/exmple/test/Sub.java
@@ -0,0 +1,7 @@
+package com.exmple.test;
+
+public class Sub {
+    public static void main(String[] args) {
+        System.out.println("Hello, Sub World!");
+    }
+}
