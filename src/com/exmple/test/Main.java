84782bb24015ef3fa68a7e0940843a59691ff31f
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 10:32:20 +0900

    Delete fields

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -1,9 +1,6 @@
 package com.exmple.test;
 
 public class Main {
-	final static String HELLO = "Hello, World!";
-	double pi = 3.141592;
-
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 
