54f88e4f9e7536c6030549a1ff4abc3be99def87
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 14:28:54 +0900

    Add sample case: "MUST avoid hard-coding."

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -1,19 +1,9 @@
 package com.exmple.test;
 
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
-		System.out.print("Please enter the price: ");
-		try (Scanner scanner = new Scanner(System.in)) {
-			String price = scanner.nextLine();
-			int priceInt = Integer.parseInt(price);
-			int tax = (int)(priceInt * 10 / 100);
-			int totalPrice = priceInt + tax;
-
-			System.out.println("Price with tax: " + totalPrice);
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-		}
+		final double circleRadius = 5.0;
+		final double ciecleArea = 3.141592 * Math.pow(circleRadius, 2);
+		System.out.println("Circle area: " + ciecleArea);
 	}
 }
