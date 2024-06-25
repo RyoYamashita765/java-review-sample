a1ab1671ee16140b1beaf6c1338ab1a5aa2fc34b
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 11:07:52 +0900

    Update some language files

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,5 +3,9 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, Main World!");
+
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("args[" + i + "] = " + args[i]);
+		}
 	}
 }
