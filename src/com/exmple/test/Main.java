f61e1d933acee4c6a948cb95c4c5c5691442b5b4
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 10:53:06 +0900

    Add lines to some files

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,5 +3,8 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello, Main World!");
+
+		Sub sub = new Sub();
+		sub.main(args);
 	}
 }
--- src/com/exmple/test/Sub.java
+++ src/com/exmple/test/Sub.java
@@ -3,5 +3,9 @@ package com.exmple.test;
 public class Sub {
     public static void main(String[] args) {
         System.out.println("Hello, Sub World!");
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
     }
 }
