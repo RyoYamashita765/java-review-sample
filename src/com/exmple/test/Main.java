91d3bcedd736b54619499419b57b0c64541c7ba2
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 10:03:18 +0900

    Add "switch" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,5 +3,17 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+
+		switch (args.length) {
+			case 0:
+				System.out.println("No arguments");
+				break;
+			case 1:
+				System.out.println("One argument");
+				break;
+			default:
+				System.out.println("More than one argument");
+				break;
+		}
 	}
 }
