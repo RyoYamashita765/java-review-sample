3bf0457583f5022c1000a6d64cdf702589b7954b
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 10:05:32 +0900

    Delete "switch" block

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,17 +3,5 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
-		switch (args.length + 1) {
-			case 0:
-				System.out.println("No arguments");
-				break;
-			case 2:
-				System.out.println("One argument");
-				break;
-			default:
-				System.out.println("More than one argument");
-				break;
-		}
 	}
 }
