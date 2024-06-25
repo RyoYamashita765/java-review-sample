ed67532e29af5d1ee4276244b03aa6fc668e5d23
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 10:23:02 +0900

    Add multiple lines to some points

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,6 +2,14 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
+		for (String arg : args) {
+			System.out.println("arg = " + arg);
+		}
+
 		System.out.println("Hello World!");
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("i = " + i);
+		}
 	}
 }
