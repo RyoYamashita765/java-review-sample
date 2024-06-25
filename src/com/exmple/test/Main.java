5c3a255ca4c9bb1f0aac60a1049d37a486a9f80f
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 14:45:55 +0900

    Add sample case: "Define an interface when introducing a new class."

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -1,16 +1,51 @@
 package com.exmple.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 	public static void main(String[] args) {
-		CsvFileReader csvFileReader = new CsvFileReader();
-		String csvFileContent = csvFileReader.read("foobar.csv");
-		System.out.println(csvFileContent);
+		DatabaseManager databaseManager = new DatabaseManager("jdbc:mysql://localhost:3306/test", "admin", "pass");
+		List<Map<String, String>> result = databaseManager.select("SELECT * FROM test_table");
+		result.forEach(System.out::println);
 	}
 }
 
-class CsvFileReader {
-	public String read(String path) {
-		// Dummy
-		return "csv file content";
+class DatabaseManager {
+	private String url;
+	private String username;
+	private String password;
+
+	public DatabaseManager(String url, String username, String password) {
+		this.url = url;
+		this.username = username;
+		this.password = password;
 	}
+
+	public List<Map<String, String>> select(String query) {
+		List<Map<String, String>> output = new ArrayList<>();
+		try (Connection connection = DriverManager.getConnection(url, username, password);
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery(query)) {
+			ResultSetMetaData metaData = resultSet.getMetaData();
+			while (resultSet.next()) {
+				Map<String, String> row = new HashMap<>();
+				for (int i = 1; i <= metaData.getColumnCount(); i++) {
+					row.put(metaData.getColumnName(i), resultSet.getString(i));
+				}
+				output.add(row);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return output;
+    }
 }
