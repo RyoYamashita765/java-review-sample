8ac74ac12b743124df3ac06a00b422d25c7d96a5
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 10:27:24 +0900

    Add method

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -3,5 +3,11 @@ package com.exmple.test;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+
+		printHello();
+	}
+
+	public static void printHello() {
+		System.out.println("Hello");
 	}
 }
