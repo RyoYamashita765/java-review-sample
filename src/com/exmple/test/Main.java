1c9952b40e21678a42e5c52b024cec7c10984b75
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 10:25:13 +0900

    Delete multiple lines to some points

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,17 +2,6 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
-		for (int i = 0; i < args.length; i++) {
-			System.out.println("[index] " + i);
-			System.out.println("[arg] " + args[i]);
-		}
-
 		System.out.println("Hello World!");
-
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < i; j++) {
-				System.out.print("i, j = " + i + ", " + j + " ");
-			}
-		}
 	}
 }
