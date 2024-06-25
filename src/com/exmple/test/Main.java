43c079a03e060b0e3d5f3ed4b8babc2a4cc03cea
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 10:24:54 +0900

    Update multiple lines to some points

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -2,14 +2,17 @@ package com.exmple.test;
 
 public class Main {
 	public static void main(String[] args) {
-		for (String arg : args) {
-			System.out.println("arg = " + arg);
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("[index] " + i);
+			System.out.println("[arg] " + args[i]);
 		}
 
 		System.out.println("Hello World!");
 
 		for (int i = 0; i < 10; i++) {
-			System.out.println("i = " + i);
+			for (int j = 0; j < i; j++) {
+				System.out.print("i, j = " + i + ", " + j + " ");
+			}
 		}
 	}
 }
