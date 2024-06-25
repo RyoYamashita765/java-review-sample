a16cc9d6032dabc5f785676913098a1851b0e1c9
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-25 10:28:52 +0900

    Delete one line from multiple methods

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -4,12 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 
-		System.out.print("printHello(): ");
 		printHello();
 	}
 
 	public static void printHello() {
-		System.out.println("Hello,");
 		System.out.println(" World");
 	}
 }
