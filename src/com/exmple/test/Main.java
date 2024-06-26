653e07ea7b8f32350e698c4b6eb1e209d1fb0e5d
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-26 18:11:23 +0900

    Add sample case: "import statements should be the alphabetical order."

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -1,7 +1,38 @@
 package com.exmple.test;
 
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.time.ZonedDateTime;
+import com.exmple.test.sub.Sub;
+import guava.Guava;
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		list.add("Hello");
+		list.add("World");
+		list.add("Java");
 
+		for (String str : list) {
+			System.out.println(str);
+		}
+
+		list.sort((a, b) -> a.compareTo(b));
+		for (String str : list) {
+			System.out.println(str);
+		}
+
+		Map<String, Integer> map = new HashMap<>();
+		map.put("Hello", 1);
+		map.put("World", 2);
+		map.put("Java", 3);
+
+		for (String key : map.keySet()) {
+			System.out.println(key + " : " + map.get(key));
+		}
 	}
 }
