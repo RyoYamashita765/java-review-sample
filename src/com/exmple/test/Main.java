390b72b2373a22a82bd415cf1de2f7c04d3d5015
Ryo Yamashita <yamashita@ipride.co.jp>
2024-08-23 17:20:11 +0900

    Add sample case: "Follow Java coding rules for class, variable and constant names."

--- src/com/exmple/test/Main.java
+++ src/com/exmple/test/Main.java
@@ -1,38 +1,15 @@
-package com.exmple.test;
-
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.time.ZonedDateTime;
-import com.exmple.test.sub.Sub;
-import guava.Guava;
-import java.util.Map;
-import java.util.List;
-import java.util.HashMap;
-import java.util.ArrayList;
-
 public class Main {
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<>();
-		list.add("Hello");
-		list.add("World");
-		list.add("Java");
+		var user_name = "John Smith";
+		var SUB_ROUTINE = new suBroutIne();
+		SUB_ROUTINE.Print_hello(user_name);
+	}
+}
 
-		for (String str : list) {
-			System.out.println(str);
-		}
+class suBroutIne {
+	final static String StringPhrase = "Hello, ";
 
-		list.sort((a, b) -> a.compareTo(b));
-		for (String str : list) {
-			System.out.println(str);
-		}
-
-		Map<String, Integer> map = new HashMap<>();
-		map.put("Hello", 1);
-		map.put("World", 2);
-		map.put("Java", 3);
-
-		for (String key : map.keySet()) {
-			System.out.println(key + " : " + map.get(key));
-		}
+	public static void Print_hello(String Name) {
+		System.out.println(suBroutIne.StringPhrase + Name);
 	}
 }
